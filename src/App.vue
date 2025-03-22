b3fbacbb06394489bca60959cbb97d51a5ad04ad
Rizkyalii <ikymlnaali09@gmail.com>
2025-03-22 02:49:18 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref } from 'vue';
+const count = ref(0)
+const nama = ref("")
+
+
+const gambar1 = "/public/favicon.ico"
+const gambar2 = "/src/assets/logo.svg"
+
+const gambar = ref(gambar1)
+const altText = ref("Gambar Pertama")
+
+const ubahGambar = () =>{
+  if(gambar.value === gambar1){
+    gambar.value =gambar2
+    altText.value = "Gambar Kedua"
+  }else{
+    gambar.value = gambar1
+    altText.value = "Gambar pertama"
+  }
+}
+
+const loggedIn = ref(false)
+
+const login = () =>{
+  loggedIn.value = true
+}
+
+const logOut = () => {
+  loggedIn.value=false
+}
+
+</script>
+
+<template>
+<div>{{ count }}</div>
+<div>Halo,{{ nama }}</div>
+
+<div>
+  <label for="name">Input Nama :</label>
+  <input type="text" v-model="nama" placeholder="Tuliskan nama anda">
+</div>
+
+
+<div>
+  <button @click="count++">
+    Count
+  </button>
+</div>
+
+<div>
+  <h1>{{ altText }}</h1>
+  <img :src="gambar" :alt="altText" width="100px">
+  <button @click="ubahGambar">Switch</button>
+</div>
+
+<div>
+  <h1>V-if dan v-else</h1>
+  <p v-if="loggedIn">
+    Berhasil Login
+    <img src="/src//assets/logo.svg" alt="" width="100px">
+    <button @click="logOut">LogOut</button>
+  </p>
+  <p v-else>
+    Login Dulu
+    <button @click="login">Login</button>
+  </p>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pertemuan5.vue
@@ -0,0 +1,89 @@
+<script setup>
+
+import { ref,computed,watch,onMounted } from 'vue';
+
+const activity =ref("")
+
+const data =ref ([
+    {id:1,nama:"ali",done:true},
+    {id:2,nama:"ale",done:false},
+    {id:3,nama:"agus",done:true}
+
+]);
+
+const addData = () => {
+    let newData = {
+        id:data.value.length+1,
+        nama:activity.value,
+        done:false
+    }
+
+    data.value.push(newData)
+    activity.value = ""
+}
+
+
+const doneData = computed ( () => {
+    return data.value.filter( (todo) => todo.done ==true)
+})
+const notDoneData = computed ( () => {
+    return data.value.filter( (todo) => todo.done ==false)
+})
+
+const number = ref(0);
+const refButton = ref("");
+
+watch(number,(a,b) =>{
+    if(a>=5){
+        console.log("Number  Changed",a,b)
+        console.log(refButton);
+        
+    }
+})
+
+
+const forMounted = ref(null);
+
+onMounted( () =>{
+    forMounted.value.textContent ="Ini bukan pertemuan 5";
+})
+
+</script>
+
+<template>
+<h1>Pertemuan 5</h1>
+<h1 ref="forMounted">Ini Pertemuan 5</h1>
+
+<p>Count :{{ number }}</p>
+<button @click="number++">Klik</button>
+
+
+<p>New Data :{{ activity }}</p>
+<input v-model="activity">
+<button @click="addData">Submit</button>
+
+
+<h1>Semua Data</h1>
+<ul>
+    <li v-for="todo in data" :key="todo.id">{{ todo.id }}. {{ todo.nama }} - {{ todo.done }}
+        <input type="checkbox" v-model="todo.done">
+    </li>
+</ul>
+<h1>Sudah Terdata</h1>
+<ul>
+    <li v-for="todo in doneData" :key="todo.id">{{ todo.id }}. {{ todo.nama }} - {{ todo.done }}
+        <input type="checkbox" v-model="todo.done">
+    </li>
+</ul>
+<h1>Belum  Terdata</h1>
+<ul>
+    <li v-for="todo in notDoneData" :key="todo.id">{{ todo.id }}. {{ todo.nama }} - {{ todo.done }}
+        <input type="checkbox" v-model="todo.done">
+
+    </li>
+</ul>
+
+</template>
+
+<style>
+</style>
